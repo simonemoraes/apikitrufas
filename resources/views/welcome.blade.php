767c7b85bd54ede8090b384d85ccfbc63f5f24fa
simone louzada moraes <simone.moraes77@gmail.com>
2018-02-16 18:25:17 -0200

    teste 3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Kitrufas
                 </div>
 
                 <div class="links">
