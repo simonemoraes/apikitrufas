9d32cc8195a3a02dd6aac62348d81a5f59f8fadd
simon <simone.moraes77@gmail.com>
2018-02-20 22:05:01 -0300

    Instalando o cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
-        \Barryvdh\Cors\HandleCors::class,
+        \Barryvdh\Cors\HandleCors::class
     ];
 
     /**
@@ -41,6 +41,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
+            \Barryvdh\Cors\HandleCors::class,
 
         ],
     ];
@@ -60,6 +61,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'cors'=>\Barryvdh\Cors\HandleCors::class,
+        'cors'=>\Barryvdh\Cors\HandleCors::class
     ];
 }
